4db0109f1c38f0d09d53e4b4f51700bf5cbc1778
Muhammad Saad <saadfenixel@gmail.com>
2026-02-01 19:47:18 +0500

    Delete 9) filing.cpp

--- 9) filing.cpp
+++
@@ -1,8 +0,0 @@
-#include <fstream>           // {}
-#include <iostream>          // {10} cin, get, getline, ws, good, fail, clear, ignore, cout, endl
-using std::cin, std::cout;
-
-
-int main(){
-
-}
